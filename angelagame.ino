e72c5d2eaf369b1dd3983ec9ecded877c5d6c72a
nicolotrinca <46709178+nicolotrinca@users.noreply.github.com>
2019-02-17 11:25:49 +0100

    Add files via upload

--- angelagame.ino
+++ angelagame.ino
@@ -28,9 +28,168 @@ void setup() {
   lcd.print("Scegli l arrivo");
   pinMode(meno, INPUT); //meno
   pinMode(conferma, INPUT); //conferma
-  pinMode(piu, INPUT); //più
+  pinMode(piu, INPUT); //piÃ¹
   digitalWrite(meno, HIGH);
   digitalWrite(conferma, HIGH);
   digitalWrite(piu, HIGH);
   setArrivo();
+}
+void setArrivo()
+{
+  lcd.setCursor(7, 2);
+  lcd.print(arrivo);
+  
+  while (digitalRead(conferma) == HIGH )
+  {
+    if (digitalRead(meno) == LOW)
+    {
+      if (arrivo == 100)
+      {
+        lcd.setCursor(9, 2);
+        lcd.print(" ");
+      }
+      if (arrivo > 30)
+      {
+        arrivo--;
+      }
+      lcd.setCursor(7, 2);
+      lcd.print(arrivo);
+      delay(200);
+    }
+    if (digitalRead(piu) == LOW)
+    {
+      if (arrivo < 100)
+      {
+        arrivo++;
+      }
+      
+      lcd.setCursor(7, 2);
+      lcd.print(arrivo);
+      delay(200);
+    }
+  }
+  lcd.setCursor(7, 2);
+  lcd.print(arrivo);
+  }
+  void Valore(int v,int giocatore)
+  {
+    lcd.clear();
+    lcd.setCursor(9, 2);
+    lcd.print("tot:");
+    lcd.setCursor(2, 2);
+    lcd.print("n:");
+    lcd.setCursor(4, 2);
+    lcd.print(v);
+    lcd.setCursor(13, 2);
+    lcd.print(tot);
+    delay(200);
+    lcd.setCursor(0, 0);
+    lcd.print("Scegli un valore");
+  while (digitalRead(conferma) == HIGH )
+  {
+    if (digitalRead(meno) == LOW)
+    {
+    
+      if (v > 0)
+      {
+        v--;
+      }
+      lcd.setCursor(4, 2);
+      lcd.print(v);
+      lcd.setCursor(13, 2);
+      lcd.print(tot);
+      delay(200);
+    }
+    if (digitalRead(piu) == LOW)
+    {
+      if (v < 6)
+      {
+        v++;
+      }
+      
+      lcd.setCursor(7, 2);
+       lcd.setCursor(4, 2);
+      lcd.print(v);
+      lcd.setCursor(13, 2);
+      lcd.print(tot);
+      delay(200);
+    }
+  }
+  
+    if(v == ValorePrecedente)
+    {
+    Valore(v,giocatore);
+    return;
+    }
+    if(7-ValorePrecedente==v)
+    {
+      Valore(v,giocatore);
+      return;
+    }
+    if(prima==false&&v==0)
+    {
+      Valore(v,giocatore);
+      return;
+    }
+    
+  lcd.setCursor(4, 2);
+      lcd.print(v);
+      lcd.setCursor(13, 2);
+      lcd.print(tot);
+      delay(200);
+  Verifica(v,giocatore);
+  tot+=v;
+  ValorePrecedente=v;
+  prima=false;
+    }
+  void Gioca()
+  {
+    while(tot<arrivo)
+    {
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       giocatore=1;
+       lcd.print("Giocatore ");
+       lcd.print(giocatore);
+       delay(1000);
+       Valore(v1,giocatore);
+       lcd.clear();
+       lcd.setCursor(0, 0);
+       giocatore=2;
+       lcd.print("Giocatore ");
+       lcd.print(giocatore);
+       delay(1000);
+       Valore(v2,giocatore);
+    }
+    }
+    void Verifica(int v,int giocatore)
+    {
+      if(tot+v==arrivo)
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Il vincitore e");
+        lcd.setCursor(0, 1);
+        lcd.print("il giocatore ");
+        lcd.print(giocatore);
+        delay(2000);
+        }
+        if(tot+v>arrivo)
+        {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Il perdente e");
+        lcd.setCursor(0, 1);
+        lcd.print("il giocatore ");
+        lcd.print(giocatore);
+        delay(2000);
+          }
+      }
+      
+void loop() {
+ Gioca();
+ lcd.clear();
+ lcd.print("Nuova partita");
+ delay(2000);
+ tot=0;
 }
